12516e75530894490747c470189883c7fb0b3468
MillisamiSprout <sachin@sprout-technology.com>
2011-07-06 17:09:43 +0545

    initial payload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitty::Application.config.secret_token = 'e23a16e057730dcbfedee53148d09afed2d4677f272640734ca6cf5c49eabf38f5935a0062c0d135db0803385edac9b76bc29cb1aedaad73e74e7079e37b4f87'
